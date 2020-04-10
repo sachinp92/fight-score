f96408973c0601e47c5240393ad45b4576cc00cf
Sachin Patel <sachin.patel@hotmail.co.uk>
2020-04-10 22:16:36 +0100

    Cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,9 @@ import { Provider } from 'react-redux';
 import reducer from './store/reducers';
 import Router from './Router';
 
-const initialState = {
-  fighterOneName: '',
-  fighterTwoName: '',
-  fighterOneRoundScore: 0,
-  fighterTwoRoundScore: 0,
-  fighterOneTotal: 0,
-  fighterTwoTotal: 0,
-  currentRound: 1,
-  fightLength: 12
-};
-
-const store = createStore(reducer, initialState);
-
-const App = () => <Provider store={store}><Router /></Provider>;
+const App = () =>
+  <Provider store={createStore(reducer)}>
+    <Router />
+  </Provider>;
 
 export default App;
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,4 +1,15 @@
-const reducer = (state, action) => {
+const initialState = {
+  fighterOneName: '',
+  fighterTwoName: '',
+  fighterOneRoundScore: 0,
+  fighterTwoRoundScore: 0,
+  fighterOneTotal: 0,
+  fighterTwoTotal: 0,
+  currentRound: 1,
+  fightLength: 12
+};
+
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'hello':
 
